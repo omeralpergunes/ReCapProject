6f02d081048ee0a63952f49649d2c2d184a426a7
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-07-08 17:21:43 +0300

    Add files via upload

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService
+    {
+        IDataResult<List<Customer>> GetAll();
+
+        public IResult Add(Customer customer);
+        public IResult Update(Customer customer);
+        public IResult Delete(Customer customer);
+    }
+}
