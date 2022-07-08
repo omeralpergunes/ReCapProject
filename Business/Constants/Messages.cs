19964a619d46f215700d19ef5b5752048bd64d9b
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-07-08 17:22:24 +0300

    Add files via upload

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,20 @@ namespace Business.Constants
         public static string CarNameInvalidid = "Araç ismi geçersizdir";
         public static string MaintenanceTime = "Sistem şu an bakımdadır";
         public static string CarsListed = "Ürünler Listelendi";
+
+        public static string UserAdded="Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UsersListed = "Kullanıcı listesi açıldı";
+        public static string UserUpdated = "Kullanıcı güncellendi";
+
+        public static string CustomersAdded = "Müşteri eklendi";
+        public static string CustomersDeleted = "Müşteri silindi";
+        public static string CustomersListed = "Müşteri listesi açıldı";
+        public static string CustomersUpdated = "Müşteri silindi";
+
+        public static string RentalsAdded="Kiralama isteğiniz eklendi";
+        public static string RentalsDeleted = "Kiralama silindi";
+        public static string RentalsListed= "Kiralama listesi açıldı";
+        public static string RentalsUpdated="Kiralama güncellendi";
     }
 }
