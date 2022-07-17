ea04979b8581f75d5a823d1281a58ca9c1e604fa
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-07-17 20:34:13 +0300

    Add files via upload

--- Business/Abstract/IBrandService.cs
+++ Business/Abstract/IBrandService.cs
@@ -1,4 +1,6 @@
-﻿ using System;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,10 @@ namespace Business.Abstract
 {
     public interface IBrandService
     {
+        IResult Add(Brand brand);
+        IResult Delete(Brand brand);
+        IResult Update(Brand brand);
+        IDataResult<List<Brand>> GetAll();
+        IDataResult<List<Brand>> GetById(int id);
     }
 }
