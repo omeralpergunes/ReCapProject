18d108e6f90713fbfbc989223dd9b3e2409991f7
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-07-20 15:02:41 +0300

    Add files via upload

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,7 +12,7 @@ namespace Business.Constants
         public static string MaintenanceTime = "Sistem şu an bakımdadır";
         public static string CarsListed = "Ürünler Listelendi";
 
-        public static string UserAdded="Kullanıcı eklendi";
+        public static string UserAdded = "Kullanıcı eklendi";
         public static string UserDeleted = "Kullanıcı silindi";
         public static string UsersListed = "Kullanıcı listesi açıldı";
         public static string UserUpdated = "Kullanıcı güncellendi";
@@ -22,9 +22,22 @@ namespace Business.Constants
         public static string CustomersListed = "Müşteri listesi açıldı";
         public static string CustomersUpdated = "Müşteri silindi";
 
-        public static string RentalsAdded="Kiralama isteğiniz eklendi";
+        public static string RentalsAdded = "Kiralama isteğiniz eklendi";
         public static string RentalsDeleted = "Kiralama silindi";
-        public static string RentalsListed= "Kiralama listesi açıldı";
-        public static string RentalsUpdated="Kiralama güncellendi";
+        public static string RentalsListed = "Kiralama listesi açıldı";
+        public static string RentalsUpdated = "Kiralama güncellendi";
+
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandListed = "Marka listelendi";
+
+        public static string CarImageLimitExceded = "Araba resim ekleme limiti 5'dir.";
+        public static string CarImageAdded = "Araba resmi eklendi";
+        public static string CarImageDeleted = "Araba resmi silindi";
+        public static string CarImageUpdated = "Araba resmi güncellendi";
+        public static string CarImagesListed = "Araba resimleri listendi";
+        public static string CarImageDoesntExists = "Araba resmine ulaşılamıyor";
+
     }
 }
---
+++ Business/Constants/PathConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class PathConstant
+    {
+        public static string ImagesPath = "wwwroot\\Images\\";
+    }
+}
