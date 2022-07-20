346afc0932a0887e450cf8527f760c5f2ca22213
Ömer Alper Güneş <73720725+omeralpergunes@users.noreply.github.com>
2022-07-20 14:58:52 +0300

    Add files via upload

---
+++ Core/Utilities/Helpers/FileHelper/IFileHelper.cs
@@ -0,0 +1,14 @@
+﻿using Core.Utilities.Results;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Core.Utilities.Helpers.FileHelper
+{
+    public interface IFileHelper
+    {
+        IResult Delete(string filePath);
+        IResult Upload(IFormFile file, string root);
+        IResult Update(IFormFile file, string filePath, string root);
+
+    }
+}
